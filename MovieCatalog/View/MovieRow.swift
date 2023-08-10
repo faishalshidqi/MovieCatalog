f4e1bec47b5a3ddabd9d2c8b12934719ee53a6a7
faishalshidqi <rshidqi6@gmail.com>
2023-08-10 19:26:42 +0700

    remove unnecessary comments

--- MovieCatalog/View/MovieRow.swift
+++ MovieCatalog/View/MovieRow.swift
@@ -3,32 +3,6 @@ import SDWebImageSwiftUI
 
 struct MovieRow: View {
     let movie: MoviesModel
-    /*private let pendingOperations = PendingOperations()
-    
-    fileprivate func toggleSuspendOperations(isSuspended: Bool) {
-        pendingOperations.downloadQueue.isSuspended = isSuspended
-    }
-    
-    fileprivate func startDownload(movie: MoviesModel, indexPath: IndexPath) {
-        guard pendingOperations.downloadInProgress[indexPath] == nil else {return}
-        let downloader = ImageDownloader(movie: movie)
-            
-        downloader.completionBlock! {
-            if downloader.isCancelled { return }
-            DispatchQueue.main.async {
-                self.pendingOperations.downloadInProgress.removeValue(forKey: indexPath)
-            }
-        }
-        
-        pendingOperations.downloadInProgress[indexPath] = downloader
-        pendingOperations.downloadQueue.addOperation(downloader)
-    }
-    
-    fileprivate func startOperations(movie: MoviesModel, indexPath: IndexPath) {
-        if movie.state == .new {
-            startDownload(movie: movie, indexPath: indexPath)
-        }
-    }*/
     var body: some View {
         HStack {
             WebImage(url: movie.poster)
